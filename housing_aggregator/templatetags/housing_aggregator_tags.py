13d641ab2a7aaee4bad27621f3d7434868e388a0
Asan <dzhuzbaevasan@gmail.com>
2022-11-17 05:49:13 +0600

    Update templates

--- housing_aggregator/templatetags/housing_aggregator_tags.py
+++ housing_aggregator/templatetags/housing_aggregator_tags.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.simple_tag()
 def menu_items():
     return ['Home', 'Rent', 'FAQs', 'About']
+
+
+@register.simple_tag()
+def images(images):
+    return images[1:-1].split(',')
